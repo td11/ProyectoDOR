04159a2da73afaa18101d795465ccaee39611881
td11 <33686827+td11@users.noreply.github.com>
2018-03-03 18:22:29 +0000

    Add files via upload

---
+++ JS/butacas.js
@@ -0,0 +1,181 @@
+//VARIABLES GLOBALES
+var listadoButacas = [];
+const FILAS = 2,
+    COLUMNAS = 4;
+var cantidadButacas = 8;
+var butacasSeleccionadas = 0;
+
+
+
+/**
+ * Carga los datos del fichero JSON externo para modificar el estado de los asientos.
+ */
+function cargaReservas() {
+
+    var cantidadAsientos, almacenamiento = (localStorage.length == 0);
+    (almacenamiento ? cantidadAsientos = (COLUMNAS * FILAS) : cantidadAsientos = localStorage.length);
+
+    for (let i = 1; i <= cantidadButacas; i++) {
+        if (!almacenamiento) {
+            let obj = JSON.parse(localStorage.getItem('objeto' + i));
+            listadoButacas.push({
+                'iden': obj.iden,
+                'clase': obj.clase
+            });
+        } else {
+            listadoButacas.push({
+                'iden': 'butaca' + i,
+                'clase': 'disponible'
+            });
+        }
+    }
+}
+
+/**
+ * Cargamos las butacas
+ */
+function cargaEscenario() {
+
+    var svgNS = "http://www.w3.org/2000/svg";
+    var svg = document.createElementNS(svgNS, "svg");
+    var xlink = "http://www.w3.org/1999/xlink";
+
+    let contenedor = document.getElementById('butaca');
+
+    let contador = 1;
+
+    var rect = document.createElementNS(svgNS, 'rect');
+    rect.setAttribute('style', 'stroke:#000000;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:stroke fill markers');
+    rect.setAttribute('id', 'rect817');
+    rect.setAttribute('x', 0);
+    rect.setAttribute('y', 10);
+    rect.setAttribute('width', 60);
+    rect.setAttribute('height', 20);
+    rect.setAttribute('ry', '4.4099426');
+
+    svg.appendChild(rect);
+
+    rect = document.createElementNS(svgNS, 'rect');
+    rect.setAttribute('style', 'stroke:#000000;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:stroke fill markers');
+    rect.setAttribute('id', 'rect821');
+    rect.setAttribute('x', 10);
+    rect.setAttribute('y', 3);
+    rect.setAttribute('width', 46);
+    rect.setAttribute('height', 26);
+    rect.setAttribute('ry', '4.2763014');
+
+    svg.appendChild(rect);
+
+    for (let i = 1; i <= FILAS; i++) {
+        for (let j = 1; j <= COLUMNAS; j++) {
+            var butaca = document.createElementNS("http://www.w3.org/2000/svg", "use");
+
+            butaca.setAttribute('id', 'butaca' + contador);
+            butaca.setAttribute('title', i + '-' + j);
+            butaca.setAttribute('class', listadoButacas[contador - 1].clase);
+            butaca.setAttribute('y', (i * 95));
+            butaca.setAttribute('x', (j * 95));
+
+
+            butaca.setAttributeNS(xlink, 'xlink:href', '#layer1');
+            butaca.appendChild(svg);
+
+            contenedor.appendChild(butaca);
+            contador++;
+        }
+    }
+}
+
+/**
+ * Carga los eventos para cada butaca
+ */
+function cargaEventos() {
+
+    document.getElementById('guardar').addEventListener('click', function () {
+        contarNumeroButacasReservadas();
+        gestionReservas('guardar');
+    }, false);
+
+    document.getElementById('limpiar').addEventListener('click', function () {
+        gestionReservas('limpiar');
+    }, false);
+    
+
+    for (let i = 1; i <= listadoButacas.length; i++) {
+        document.getElementById('butaca' + i).addEventListener('click', function () {
+            let clase = this.getAttribute('class');
+
+            switch (clase) {
+                case 'disponible':
+                    this.setAttribute('class', 'seleccionado');
+                    break;
+                case 'seleccionado':
+                    this.setAttribute('class', 'disponible');
+                    break;
+                case 'reservado':
+                    alert('Este asiento esta reservado');
+                    butacasSeleccionadas = butacasSeleccionadas + 1;
+                    break;
+            }
+
+        }, false);
+    }
+
+}
+
+
+/**
+ * Guarda o cancela las reservas.
+ */
+function gestionReservas(opcion) {
+
+    for (let i = 1; i <= cantidadButacas; i++) {
+        let elem = document.getElementById('butaca' + i);
+        let clase = elem.getAttribute('class');
+        if (opcion == 'guardar') {
+            if (clase == 'seleccionado') {
+                elem.setAttribute('class', 'reservado');
+            }
+        } else {
+            elem.setAttribute('class', 'disponible');
+        }
+        var obj = {
+            'iden': elem.id,
+            'clase': elem.getAttribute('class')
+        };
+        localStorage.setItem('objeto' + i, JSON.stringify(obj));
+    }
+
+    $('#contenedorButacas').hide();
+    $('#formularioPago').show();
+}
+
+function contarNumeroButacasReservadas(){
+    var butacasReservadas = $('#butaca .seleccionado');
+    butacasSeleccionadas = butacasReservadas.length;
+}
+
+/* Mostrar detalles de pago */
+function mostrarDetallesDePago() {
+    var precioValor = precioSeleccionado.split('-');
+    var contenedor = $('#confirmacionPago');
+    var titulo = $('<h3>Confirmacion de pago</h3>');
+    var pagoTotal = $('<span>Pago a realizar: ' + parseFloat(precioValor[1]) * parseFloat(butacasSeleccionadas) + '</span><br>');
+    var numeroDeEntradas = $('<span>Numero de entradas: ' + butacasSeleccionadas + '</span><br>');
+    var pagoPorEntrada = $('<span>Precio de entrada: ' + precioSeleccionado + '</span><br>');
+    $('#formularioPago').hide();
+    var d = new Date();
+    var month = d.getMonth() + 1;
+    var day = d.getDate();
+    var fecha = d.getFullYear() + '/' +
+        (month < 10 ? '0' : '') + month + '/' +
+        (day < 10 ? '0' : '') + day;
+    var horario = $('<span>Fecha:'+fecha+' horario:'+horarioSeleccionado+'</span><br>');
+    contenedor.append(titulo);
+    contenedor.append(numeroDeEntradas);
+    contenedor.append(pagoPorEntrada);
+    contenedor.append(pagoTotal);
+    contenedor.append(horario);
+    contenedor.show();
+
+}
---
+++ JS/javascript.js
@@ -0,0 +1,210 @@
+var titulopelicula = "";
+var id = "";
+var horarios = "";
+var precios = "";
+let precioSeleccionado, horarioSeleccionado;
+var btnReservarYa = $('#btnReservarYa');
+
+/* Inicio */
+$(function () {
+
+    cargaReservas();
+    $("#contenedorButacas").hide();
+    $('#formularioPago').hide();
+    
+
+    $('img').keyup(function (e) {
+        if (e.keyCode == 13) {
+            ventanaModal(this);
+        }
+    });
+
+    $('.close').keyup(function (e) {
+        if (e.keyCode == 13) {
+            cerrarModal();
+        }
+    });
+
+    $('#btnIrButacas').on('click', function (event) {
+        esconderContenidoModal();
+        mostrarReservaEntradas();
+        llamadaButacas();
+        horarioSeleccionado = $('#horarios select option:selected').text();
+        precioSeleccionado = $('#precios select option:selected').text();
+
+    });
+
+    btnReservarYa.on('click', function (event) {
+        mostrarDetallesDePago();
+    });
+
+
+});
+
+//Llamada a funciones de butacas
+function llamadaButacas() {
+    //cargaEscenario();
+    cargaEventos();
+}
+
+
+
+/* Reserva de entradas */
+function mostrarReservaEntradas() {
+    var contenedorButacas = $('#contenedorButacas');
+    contenedorButacas.show();
+}
+
+
+/* Cargar ventana modal */
+function ventanaModal(elemento) {
+    limpiarVentanaModal();
+    id = $(elemento).attr("id");
+    $("body .modal").css('display', 'block');
+    $.getJSON("JS/datosimagenes.json", function (json) {
+        horarios = json.datosImagenes[parseInt(id)].horario.split("/");
+        precios = json.datosImagenes[parseInt(id)].precio.split("/");
+        $(".modal-header #tituloPelicula").append("<span>" + json.datosImagenes[parseInt(id)].titulo + "</span>");
+        $(".modal-body #sinopsis").append("<p>" + json.datosImagenes[parseInt(id)].sinopsis + "</p>");
+        llenarComboHorarios();
+        llenarComboPrecios();
+    });
+}
+
+function llenarComboHorarios() {
+    var combo = $('#horarios select');
+    for (var i = 0; i < horarios.length; i++) {
+        var select = $('<option></option>');
+        var span = horarios[i];
+        select.append(span);
+        select.attr('value', horarios[i]);
+        combo.append(select);
+    }
+}
+
+function llenarComboPrecios() {
+    var combo = $('#precios select');
+    for (var i = 0; i < precios.length; i++) {
+        var select = $('<option></option>');
+        var span = precios[i];
+        var precioValor = precios[i].split('-');
+        select.append(span);
+        select.attr('value', precioValor[1]);
+        combo.append(select);
+    }
+}
+
+
+
+/* Cerrar modal */
+function cerrarModal() {
+    $("body .modal").css('display', 'none');
+    $("#contenido form").css('display', 'block');
+    $('#contenedorButacas').hide();
+    $('#formularioPago').hide();
+    $('#confirmacionPago').hide();
+    $('#confirmacionPago').children().remove();
+    $('#sinopsis').show();
+    $('#horarios').show();
+    $('#precios').show();
+    $('#btnIrButacas').show();
+}
+
+/* Vaciar ventana modal */
+function limpiarVentanaModal() {
+    $(".modal-header #tituloPelicula span").remove();
+    $("#contenido p").remove();
+}
+
+function esconderContenidoModal() {
+    $("#contenido").children().hide();
+}
+
+/* Slider */
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}
+
+
+/* Graficos */
+
+
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChart() {
+
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Topping');
+    data.addColumn('number', 'Slices');
+    data.addRows([
+          ['Blade Runner 2049', votos[0]],
+          ['John Wick 2', votos[1]],
+          ['Jumanji', votos[2]],
+          ['La Liga de la Justicia', votos[3]],
+          ['El Rey Arturo', votos[4]],
+          ['Logan', votos[5]],
+          ['Mazinger Z', votos[6]],
+          ['Star Wars TLJ', votos[7]],
+          ['Thor Ragnarok', votos[8]],
+          ['Valerian', votos[9]]
+    ]);
+
+    // Set chart options
+    var options = {
+        'title': 'Votaciones Mejor Pelicula del año',
+        'width': 400,
+        'height': 300
+    };
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+}
+
+// dibujar grafico donut
+function drawDonut() {
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Topping');
+    data.addColumn('number', 'Slices');
+    data.addRows([
+          ['Blade Runner 2049', votos[0]],
+          ['John Wick 2', votos[1]],
+          ['Jumanji', votos[2]],
+          ['La Liga de la Justicia', votos[3]],
+          ['El Rey Arturo', votos[4]],
+          ['Logan', votos[5]],
+          ['Mazinger Z', votos[6]],
+          ['Star Wars TLJ', votos[7]],
+          ['Thor Ragnarok', votos[8]],
+          ['Valerian', votos[9]]
+    ]);
+
+    var options = {
+        title: 'Votos mejor pelicula del año segundo grafico',
+        pieHole: 0.4,
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+    chart.draw(data, options);
+}
